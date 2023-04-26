a69bc88065e7094fd9b7715c0d029109bb0f9920
root <root@ip-172-31-39-76.ap-south-1.compute.internal>
2023-04-26 10:27:21 +0000

    project added

---
+++ ExampleProject/build/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/unit-test/ExampleProject/lib/googletest
+# Build directory: /mnt/unit-test/ExampleProject/build/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ ExampleProject/build/src/CMakeFiles/ExampleProject_lib.dir/Formula.cpp.o.d
@@ -0,0 +1,3 @@
+src/CMakeFiles/ExampleProject_lib.dir/Formula.cpp.o: \
+ /mnt/unit-test/ExampleProject/src/Formula.cpp /usr/include/stdc-predef.h \
+ /mnt/unit-test/ExampleProject/src/Formula.h
