b2fd632cc05a4b4f03a2c9ec093bbba9c308688c
shredhanbhar <73171971+shredhanbhar@users.noreply.github.com>
2023-04-27 10:04:27 +0530

    Delete tool.sh

--- tool.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-sh 'sudo apt-get update'
-sh 'sudo apt-get install git -y'            
-sh 'sudo apt-get install cmake -y'                
-sh 'sudo apt-get install make -y'
-sh 'sudo apt-get install -y build-essential'
